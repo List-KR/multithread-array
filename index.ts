90f1d6ceca872fc51554b5c5d59f2d01cbaf46ab
piquark6046 <piquark6046@proton.me>
2024-03-27 09:26:13 +0000

    add init index.ts

---
+++ index.ts
@@ -0,0 +1,31 @@
+export type MultithreadArrayOptions = {
+  /**
+   * A count of the number of threads to use.
+   * The number should be integer and greater than 0.
+   * @example
+   * ```
+   * typeof window === 'undefined' ? os.cpus().length : navigator.hardwareConcurrency
+   * ```
+   */
+  Count: number
+}
+
+function ProcessMultithreadArrayParameters(Options: MultithreadArrayOptions) {
+  if (!Number.isInteger(Options.Count)) {
+    throw new Error('MultithreadArrayOptions.Count should be an integer')
+  }
+  if (Options.Count <= 0) {
+    throw new Error('MultithreadArrayOptions.Count should be greater than 0')
+  }
+}
+
+export function MultithreadArray<T>(ArrayPara: T[], Options: MultithreadArrayOptions): T[][] {
+  ProcessMultithreadArrayParameters(Options)
+  
+  const Result: T[][] = []
+  for (var I = 0; I < Options.Count; I++) {
+    Result.push(ArrayPara.slice(I * Math.floor(ArrayPara.length / Options.Count), (I + 1) * Math.floor(ArrayPara.length / Options.Count)))
+  }
+
+  return Result
+}
