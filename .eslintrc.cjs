9b6c6e3cff9b45379c00c65427fcbeb61c0d8181
piquark6046 <piquark6046@proton.me>
2024-03-27 09:25:10 +0000

    Lint: add ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: './tsconfig.json'
+	},
+	plugins: ['@typescript-eslint', '@stylistic'],
+	rules: {
+		'@typescript-eslint/naming-convention': ['error', {
+			selector: ['variableLike', 'parameterProperty', 'classProperty', 'typeProperty'],
+			format: ['PascalCase']
+		}],
+		'@typescript-eslint/prefer-nullish-coalescing': 'off',
+		'new-cap': 'off',
+    'no-var': 'off',
+		'@stylistic/indent': ['off', 'tab'],
+		'@stylistic/semi': ['error', 'never'],
+		'@stylistic/comma-dangle': ['error', 'never'],
+		'@stylistic/comma-spacing': ['error', { before: false, after: true }],
+		'@stylistic/quotes': ['error', 'single'],
+	},
+	ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs']
+}
