c72ea08852eacae022b9ea8c74adfa24ebf08de9
PiQuark6046 <piquark6046@proton.me>
2024-03-28 07:24:19 +0000

    1.0.0 (#1)
    * ci: add GitHub workflows
    
    * Lint: add ESLint
    
    * chore: add .gitignore
    
    * chore: add package.json and tsconfig.json
    
    * add init index.ts
    
    * Lint: add dependabot
    
    * fix: handle remainder
    
    * ci: add npm publishing
    
    * docs: Add README
    
    * docs: add JSDoc of MultithreadArray
    
    * build: modify build config

---
+++ README.md
@@ -0,0 +1,4 @@
+# Multithread-array
+Splits an array into multiple subarrays based on the specified options.
+The reminder elements are added into the subarrays in order.
+Works in Node.js and web browsers.
---
+++ index.ts
@@ -0,0 +1,44 @@
+export type MultithreadArrayOptions = {
+  /**
+   * A count of the number of threads to use.
+   * The number should be integer and greater than 0.
+   * @example
+   * ```
+   * typeof window === 'undefined' ? os.cpus().length : navigator.hardwareConcurrency
+   * ```
+   */
+  Count: number
+}
+
+function ProcessMultithreadArrayParameters(Options: MultithreadArrayOptions) {
+  if (!Number.isInteger(Options.Count)) {
+    throw new Error('MultithreadArrayOptions.Count should be an integer')
+  }
+  if (Options.Count <= 0) {
+    throw new Error('MultithreadArrayOptions.Count should be greater than 0')
+  }
+}
+
+/**
+ * Splits an array into multiple subarrays based on the specified options.
+ * The reminder elements are added into the subarrays in order.
+ * @template T - The type of elements in the array.
+ * @param ArrayPara - The array to be split.
+ * @param Options - The options for splitting the array.
+ * @returns An array of subarrays, each containing a portion of the original array.
+ */
+export function MultithreadArray<T>(ArrayPara: T[], Options: MultithreadArrayOptions): T[][] {
+  ProcessMultithreadArrayParameters(Options)
+  
+  const Result: T[][] = []
+  for (var I = 0; I < Options.Count; I++) {
+    Result.push(ArrayPara.slice(I * Math.floor(ArrayPara.length / Options.Count), (I + 1) * Math.floor(ArrayPara.length / Options.Count)))
+  }
+
+  ArrayPara = ArrayPara.slice(Options.Count * Math.floor(ArrayPara.length / Options.Count))
+  for (var J = 0; J < ArrayPara.length; J++) {
+    Result[J].push(ArrayPara[J])
+  }
+
+  return Result
+}
